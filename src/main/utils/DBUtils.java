dab7b406a8c22aa51f7f14d4092f63810ea46eb3
qingkemian <2060097866@qq.com>
2020-12-23 09:33:41 +0800

    写入注释

--- src/main/utils/DBUtils.java
+++ src/main/utils/DBUtils.java
@@ -13,13 +13,14 @@ import java.sql.SQLException;
  */
 
 public class DBUtils {
-    private static DataSource ds=new ComboPooledDataSource();
+    // 创建数据库连接对象
+    private static DataSource ds=new ComboPooledDataSource();// 会自动去加载配置文件
 
     public static DataSource getDataSource()
     {
         return  ds;
     }
     public static Connection getConnection() throws SQLException {
-        return ds.getConnection();
+        return ds.getConnection();//获取连接对象
     }
 }
